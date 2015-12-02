e9d7d3389d44e26138aa52cbb123daa9503f434a
kylehoy <kylejameshoy@gmail.com>
2015-12-02 02:08:50 +0000

    use express

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
+var express = require('express');
+var app = express();
 
-var http = require("http"); // load the http library and call it "http"
-var server = http.createServer(function(req, res) {
-    console.log('hello');
-    
-    res.writeHead(200, {
-        'Content-Type': 'text/html'
-    });
-    res.end("welcome to UMD poops")
+app.get('/', function(req, res){
+  res.send("welcome to UMD poops");
 });
-server.listen(process.env.PORT)
+
+app.listen(process.env.PORT);
